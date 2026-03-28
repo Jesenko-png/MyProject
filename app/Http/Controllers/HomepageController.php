9290584a860bfa4c8f948bd5203b4672fa264b00
Jesenko-png <jesenko9@hotmail.com>
2026-03-28 03:38:38 +0100

    Update HomepageController.php

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomepageController extends Controller
@@ -9,10 +10,10 @@ class HomepageController extends Controller
     public function index()
     {
         $sat = date("H");
-
-
+        $products = Product::latest()->take(6)->get();
 
         $trenutnoVrijeme = date("h:i:s");
-        return view ("welcome" , compact("trenutnoVrijeme" , "sat"));
+        return view('welcome', compact('sat', 'trenutnoVrijeme' , "products"));
     }
+
 }
